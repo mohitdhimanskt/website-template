8d6ea3b212b69affa315884630149add665103db
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-09-20 22:35:50 +0530

    Commit to newsletter slideout

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -75,8 +75,8 @@ export default {
   // Newsletter Subscribe
   newsletter: {
     on: true,
-    heading: 'Subscribe to Our Newsletter',
-    btnText: 'Subscribe',
+    // heading: 'Subscribe to Our Newsletter',
+    // btnText: 'Subscribe',
     // Can be the form action on a mail chimp form, a hubspot form,
     // or any other url you want to post the form data to
     mailchimp: {
