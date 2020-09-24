7b31b34be66eceb91c9b472c7414ca4820e8c672
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-09-24 18:39:35 +0530

    Add to hiking tour image

--- components/SiteHiking.vue
+++ components/SiteHiking.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="site-hiking">
-    <h2 class="head-stl">
-      <span>HIKING TRIPS</span>
-    </h2>
-    <p class="text">
-      Hike through some of the enthralling trails in Nepal and Northern India,
-      amid luscious forests and spectacular sceneries all around. Spellbind your
-      heart and soul with our tailor made book hiking tour programs.
-    </p>
+  <div>
+    <div class="site-hiking">
+      <h2 class="head-stl">
+        <span>Hiking Trips</span>
+      </h2>
+      <p class="text">
+        Hike through some of the enthralling trails in Nepal and Northern India,
+        amid luscious forests and spectacular sceneries all around. Spellbind
+        your heart and soul with our tailor made book hiking tour programs
+      </p>
+    </div>
+    <div class="section">
+      <img
+        src="https://himachaltourism.gov.in/wp-content/uploads/2018/01/Trekkers-in-Spiti-valley-min.jpg"
+        alt="#"
+      />
+    </div>
+    <div class="template">
+      <div class="left">
+        <h3>5 Days Triund Hiking Tour Himachal Pradesh</h3>
+      </div>
+      <a class="btn primary" href="">View More</a>
+    </div>
+    <div class="form-group">
+      <img
+        src="https://lh3.googleusercontent.com/proxy/h9Zmz1ZQ612PpLG3puIjoGfx8HkVkyeqoM0rJAmJvqjizsrp70O5eHolWj4wuvZ7u7fKhSBys2w3kLXA65g7xqr60X4xcyl534DABmQDEtmvXT9UGcBR6P27Vz0BKvZtW-iwny7suCxjESlbqDOBXQ3NWw"
+        alt="#"
+      />
+    </div>
+    <div class="template-group">
+      <div class="left">
+        <h3>3 Days Pravati Hiking Tour Himachal Pradesh</h3>
+      </div>
+      <a class="btn primary" href="">View More</a>
+    </div>
   </div>
 </template>
 <script>
@@ -37,7 +63,7 @@ p {
   margin-inline-start: 0px;
   margin-inline-end: 0px;
   text-align: center;
-  color: black;
+  color: #000;
   font-size: 10px;
   margin: 15px 0;
   font-size: 23px;
