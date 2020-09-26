f35b292511a3cdd4b23f76f24ac0cd4ffb71846e
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-09-26 10:52:16 +0530

    Add to featured treks

---
+++ components/SiteFeaturedTreks.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="site-featuredtreks">
+    <h2 class="head-stl">
+      <span>Featured Trips</span>
+    </h2>
+    <p class="text">
+      There is nothing like walking to get the feel of a country. A fine
+      landscape is like a piece of music; it must be taken at the right
+      ”Discover memorable sights,stunning lakes, rich culture.
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SiteFeaturedTreks'
+}
+</script>
+<style scoped>
+h2 span {
+  display: block;
+  color: black;
+  text-align: center;
+  font-size: 30px;
+}
+.title-line h2 span {
+  font-weight: 300;
+}
+.head-stl {
+  font-weight: 700 !important;
+  font-size: 34px !important;
+}
+
+p {
+  display: block;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  text-align: center;
+  color: black;
+  font-size: 10px;
+  margin: 15px 0;
+  font-size: 23px;
+}
+</style>
