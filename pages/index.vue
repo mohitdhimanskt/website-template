93e777072b84d6977c9452df6298b5cd5ef74b94
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-10-01 12:43:30 +0530

    Add to index pages

--- pages/index.vue
+++ pages/index.vue
@@ -55,6 +55,30 @@
       image="https://i1.wp.com/www.inditrip.in/wp-content/uploads/2018/06/5509798756_c2e02970cb_z.jpg?fit=600%2C300&ssl=1"
     ></SiteHikingCard>
     <SiteTrekking></SiteTrekking>
+    <SiteTrekkingCard
+      title="6 Days Manali Trekking Tour Himachal Pradesh"
+      image="https://renokadventures.com/wp-content/uploads/2016/09/28464279894_7b42c0574c_z-1.jpg"
+    ></SiteTrekkingCard>
+    <SiteTrekkingCard
+      title="3 Days Dharamshala Trekking Tour Himachal Pradesh"
+      image="https://www.nativeplanet.com/img/2017/04/pic5-05-1491387866.jpg"
+    ></SiteTrekkingCard>
+    <SiteTrekkingCard
+      title="10 Days Kasol Trekking Tour Himachal Pradesh"
+      image="https://img.traveltriangle.com/blog/wp-content/uploads/2019/01/cover-for-10-Treks-Near-Kasol.jpg"
+    ></SiteTrekkingCard>
+    <SiteTrekkingCard
+      title="8 Days Bir Trekking Tour Himachal Pradesh"
+      image="https://res.cloudinary.com/dwzmsvp7f/image/fetch/q_75,f_auto,w_1316/https%3A%2F%2Fmedia.insider.in%2Fimage%2Fupload%2Fc_crop%2Cg_custom%2Fv1580145945%2Fdk4v76za7xz8uap92ney.jpg"
+    ></SiteTrekkingCard>
+    <SiteTrekkingCard
+      title="12 Days Tosh Trekking Tour Himachal Pradesh"
+      image="https://images.thrillophilia.com/image/upload/s--f5G2TFKw--/c_fill,f_auto,fl_strip_profile,h_600,q_auto,w_975/v1/images/photos/000/110/565/original/1492157230_Tosh_glacier.jpg.jpg?1492157230"
+    ></SiteTrekkingCard>
+    <SiteTrekkingCard
+      title="5 Days Khir Ganga Trekking Tour Himachal Pradesh"
+      image="https://www.trawell.in/admin/images/upload/454282746Kullu_Kheer_Ganga_Main.jpg"
+    ></SiteTrekkingCard>
   </div>
 </template>
 
