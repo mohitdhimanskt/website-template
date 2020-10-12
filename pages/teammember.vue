787bb3b5fc5f049d061b3d81d850f0a89a7edff5
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-10-12 20:42:53 +0530

    Change to image in team member

--- pages/teammember.vue
+++ pages/teammember.vue
@@ -3,61 +3,65 @@
     <site-hero title="Team Member" image="/uploads/team-hero.jpg"></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
-        <div class="member">
-          <img
-            src="https://prioritypixels.co.uk/wp-content/uploads/2019/09/paul-clapp-300x300.jpg"
-            alt="Paul Clapp"
-          />
-          <h2>Paul Clapp</h2>
-          <p><strong>Co-Founder & Director</strong></p>
-          <p>
-            Paul’s extensive background is in web design and marketing and he is
-            responsible for delivering each client’s vision. Paul oversees all
-            aspects of the web development cycle, from the graphic design and
-            content imagery through to technical SEO and digital strategies.
-          </p>
-        </div>
-        <div class="member">
-          <img
-            src="https://prioritypixels.co.uk/wp-content/uploads/2020/04/nathan-300x300.jpg"
-            alt="Nathan Yendle"
-          />
-          <h2>Nathan Yendle</h2>
-          <p><strong>Co-Founder & Director</strong></p>
-          <p>
-            Nathan is an accomplished digital marketeer and his vast and varied
-            experience includes running successful SEO and PPC campaigns. He
-            manages all the PPC campaigns we undertake and heads up all of our
-            digital strategy and content marketing projects.
-          </p>
-        </div>
-        <div class="member">
-          <img
-            src="https://prioritypixels.co.uk/wp-content/uploads/2020/09/team-michelle-sims-300x300.jpg"
-            alt="Michelle Sims"
-          />
-          <h2>Michelle Sims</h2>
-          <p><strong>Senior Digital Manager</strong></p>
-          <p>
-            Michelle is our Senior Digital Manager with over 12 years experience
-            in marketing and graphic design. Liaising with clients and getting
-            to know their business, she helps to develop in-depth marketing
-            strategies using both online and offline techniques to increase ROI.
-          </p>
-        </div>
-        <div class="member">
-          <img
-            src="https://prioritypixels.co.uk/wp-content/uploads/2020/09/team-danni-webber-300x300.jpg"
-            alt="Danni Webber"
-          />
-          <h2>Danni Webber</h2>
-          <p><strong>Web Developer</strong></p>
-          <p>
-            Danni is an expert web developer and PHP coder. She is obsessed with
-            details and has a keen eye for design and aesthetics. Her skills
-            help us to provide outstanding, future-proof sites for our customers
-            and deliver their digital brand goals.
-          </p>
+        <div class="template">
+          <div class="member">
+            <img
+              src="https://prioritypixels.co.uk/wp-content/uploads/2019/09/paul-clapp-300x300.jpg"
+              alt="Paul Clapp"
+            />
+            <h2>Paul Clapp</h2>
+            <p><strong>Co-Founder & Director</strong></p>
+            <p>
+              Paul’s extensive background is in web design and marketing and he
+              is responsible for delivering each client’s vision. Paul oversees
+              all aspects of the web development cycle, from the graphic design
+              and content imagery through to technical SEO and digital
+              strategies.
+            </p>
+          </div>
+          <div class="member">
+            <img
+              src="https://prioritypixels.co.uk/wp-content/uploads/2020/04/nathan-300x300.jpg"
+              alt="Nathan Yendle"
+            />
+            <h2>Nathan Yendle</h2>
+            <p><strong>Co-Founder & Director</strong></p>
+            <p>
+              Nathan is an accomplished digital marketeer and his vast and
+              varied experience includes running successful SEO and PPC
+              campaigns. He manages all the PPC campaigns we undertake and heads
+              up all of our digital strategy and content marketing projects.
+            </p>
+          </div>
+          <div class="member">
+            <img
+              src="https://prioritypixels.co.uk/wp-content/uploads/2020/09/team-michelle-sims-300x300.jpg"
+              alt="Michelle Sims"
+            />
+            <h2>Michelle Sims</h2>
+            <p><strong>Senior Digital Manager</strong></p>
+            <p>
+              Michelle is our Senior Digital Manager with over 12 years
+              experience in marketing and graphic design. Liaising with clients
+              and getting to know their business, she helps to develop in-depth
+              marketing strategies using both online and offline techniques to
+              increase ROI.
+            </p>
+          </div>
+          <div class="member">
+            <img
+              src="https://prioritypixels.co.uk/wp-content/uploads/2020/09/team-danni-webber-300x300.jpg"
+              alt="Danni Webber"
+            />
+            <h2>Danni Webber</h2>
+            <p><strong>Web Developer</strong></p>
+            <p>
+              Danni is an expert web developer and PHP coder. She is obsessed
+              with details and has a keen eye for design and aesthetics. Her
+              skills help us to provide outstanding, future-proof sites for our
+              customers and deliver their digital brand goals.
+            </p>
+          </div>
         </div>
       </template>
     </main-section>
@@ -175,7 +179,7 @@ export default {
 .opti-image-loaded + .spinner-wrapper {
   display: none;
 }
-.member {
+.template {
   display: flex;
   // grid-template-columns: repeat(auto-fit, minmax(18.75rem, 0.5fr));
   // gap: calc(var(--space) * 2);
@@ -192,7 +196,7 @@ export default {
   transition: all 0.4s ease;
   margin-right: 40px;
 }
-h3 {
+h2 {
   font-size: 18px;
   padding-bottom: 5px;
   text-transform: uppercase;
